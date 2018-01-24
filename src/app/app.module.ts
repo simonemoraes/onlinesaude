841252403422854fbdfcc539d8205f3b8e8d6860
Simone Moraes <simone.moraes@gmail.com>
2018-01-24 17:31:07 -0200

    Primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './layout/menu/menu.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ServicoComponent } from './servico/servico.component';
+import { NossotimeComponent } from './nossotime/nossotime.component';
+import { ContatoComponent } from './contato/contato.component';
+import { MapaComponent } from './contato/mapa/mapa.component';
+import { RodapeComponent } from './layout/rodape/rodape.component';
+import { PlanosComponent } from './planos/planos.component';
+import { ProdutosComponent } from './produtos/produtos.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    SobreComponent,
+    ServicoComponent,
+    NossotimeComponent,
+    ContatoComponent,
+    MapaComponent,
+    RodapeComponent,
+    PlanosComponent,
+    ProdutosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/nossotime/nossotime.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nossotime',
+  templateUrl: './nossotime.component.html',
+  styleUrls: ['./nossotime.component.css']
+})
+export class NossotimeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
